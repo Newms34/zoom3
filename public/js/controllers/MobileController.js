3576ac1c28591bf96dce804c161e95515125163c
Newms34 <Newms3450@msn.com>
2016-01-13 17:18:45 -0500

    tryin to fix mobile orientation detection

--- public/js/controllers/MobileController.js
+++ public/js/controllers/MobileController.js
@@ -12,14 +12,15 @@ app.controller("MobileController", function($scope, $window) {
         //rotObj.un = $scope.userName;
         $scope.debug = rotObj.beta;
         $scope.debugRun++;
-        socket.emit('moveData', {
+        var outObj = {
             alpha: rotObj.alpha,
             beta: -rotObj.beta,
             gamma: rotObj.gamma,
             un: $scope.userName,
             name: $scope.callMe,
             lastUpd: new Date().getTime()
-        });
+        }
+        socket.emit('moveData', outObj);
         $scope.$digest();
     }, true);
     socket.on('setAppelBak', function(nomen) {
@@ -35,26 +36,26 @@ app.controller("MobileController", function($scope, $window) {
     }, true);
     //--------------------
     //TEMPORARY, for testing, since it seems servers cannot be run on Starbucks Wifi!
-    // window.addEventListener('mousemove', function(e) {
-    //     //gotta normalize stuff first to btwn 90 and -90;
-    //     var rotObj = {
-    //         alpha: 0,
-    //         beta: -180 * (((e.y || e.clientY) / $(window).height()) - .5),
-    //         gamma: 180 * (((e.x || e.clientX) / $(document).width()) - .5),
-    //         un: $scope.userName,
-    //         name: $scope.callMe,
-    //         lastUpd: new Date().getTime()
-    //     };
-    //     socket.emit('moveData', rotObj);
-    // });
-    // window.onkeyup = function(e) {
-    //     if (e.which == 70) {
-    //         e.preventDefault();
-    //         socket.emit('fireToBack', {
-    //             un: $scope.userName
-    //         });
-    //     }
-    // };
+    window.addEventListener('mousemove', function(e) {
+        //gotta normalize stuff first to btwn 90 and -90;
+        var rotObj = {
+            alpha: 0,
+            beta: -180 * (((e.y || e.clientY) / $(window).height()) - .5),
+            gamma: 180 * (((e.x || e.clientX) / $(document).width()) - .5),
+            un: $scope.userName,
+            name: $scope.callMe,
+            lastUpd: new Date().getTime()
+        };
+        socket.emit('moveData', rotObj);
+    });
+    window.onkeyup = function(e) {
+        if (e.which == 70) {
+            e.preventDefault();
+            socket.emit('fireToBack', {
+                un: $scope.userName
+            });
+        }
+    };
 
     //beta = vel
     //gamma = turnVel.
