9e49f0524c1f437706d9dce83f20064dbc3ff365
Newms34 <Newms3450@msn.com>
2016-01-16 13:50:32 -0500

    score board

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -8,9 +8,9 @@ app.controller("MainController", function($scope, $window, playerFact) {
             if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true
         })(navigator.userAgent || navigator.vendor || window.opera);
         return check;
-    }
+    };
     if ($scope.mobilecheck()) {
-        $window.location.href = '/mobile'
+        $window.location.href = '/mobile';
     }
 
     $scope.allUsers = []; //array that holds all user objects and their current states.
@@ -30,7 +30,7 @@ app.controller("MainController", function($scope, $window, playerFact) {
                 name: $scope.callMe
             });
         }
-    }
+    };
     socket.on('nameRes', function(res) {
         //response from backend on whether this name is gud or nawt.
         if (res.good) {
@@ -45,15 +45,15 @@ app.controller("MainController", function($scope, $window, playerFact) {
             $('.userBox').css('background', '#fcc');
             var t = setTimeout(function() {
                 $('.userBox').css('background', '#fff');
-            }, 100)
+            }, 100);
         }
-    })
+    });
     window.onscroll = function(e) {
         if (!$scope.userName) {
-            $(document).scrollTop(0)
+            $(document).scrollTop(0);
             e.preventDefault();
         }
-    }
+    };
     socket.on('outData', function(data) {
         //update the data
         if ($scope.userName) {
@@ -76,7 +76,7 @@ app.controller("MainController", function($scope, $window, playerFact) {
             $scope.allUsers[whichUser].turnVel = 6 * data.gamma / 90;
             $scope.allUsers[whichUser].lastUpd = data.lastUpd;
         }
-    })
+    });
     $scope.mainTimer = setInterval(function() {
         $scope.timeElapsed++;
         $scope.playEls = $('.player');
@@ -105,7 +105,7 @@ app.controller("MainController", function($scope, $window, playerFact) {
                 }
             }
             if ($scope.allUsers[i].fireTimeLeft) {
-                $scope.allUsers[i].fireTimeLeft -= .05;
+                $scope.allUsers[i].fireTimeLeft -= 0.05;
             }
             if (whenIsIt - $scope.allUsers[i].lastUpd > $scope.maxLag) {
                 //more than 5sec have elapsed since last signal from this user's mobile
@@ -116,49 +116,55 @@ app.controller("MainController", function($scope, $window, playerFact) {
             }
         }
         $scope.$digest();
-    }, 50)
+    }, 50);
     socket.on('fire', function(fireRes) {
         var fu = $scope.allNames.indexOf(fireRes.un);
-        console.log('Firing', fu, fireRes, $scope.allUsers[fu])
         if ($scope.allUsers[fu].shotsLeft) {
             $scope.allUsers[fu].shotsLeft--;
             $scope.allUsers[fu].fireTimeLeft = 1;
             socket.emit('fireRebound', fireRes);
         }
         var possTarg = $scope.allUsers[fu].hasTarg;
-        if (possTarg!=-1){
-            console.log('TARGETTING:',$scope.allUsers[possTarg])
+        if (possTarg != -1) {
             $scope.allUsers[possTarg].hpLeft--;
-            if (!$scope.allUsers[possTarg].hpLeft){
+            if (!$scope.allUsers[possTarg].hpLeft) {
                 $scope.allUsers.splice(possTarg, 1);
                 $scope.allNames.splice(possTarg, 1);
+                socket.emit('userKill', {
+                    atk: fireRes.un
+                });
             }
-            socket.emit('hit',$scope.allUsers[possTarg]);
+            socket.emit('hit', $scope.allUsers[possTarg]);
         }
+        $scope.$digest();
+    });
+    socket.on('statsUpd', function(res) {
+        //a user was destroyed, so update score
+        var pos = $scope.allNames.indexOf(fireRes.un);
+        $scope.allUsers[pos].score++;
         $scope.$digest();
     });
     $scope.getStatNum = function(num) {
         return new Array(num);
-    }
+    };
     $scope.displayTarg = function(t) {
         if (t == -1) {
             return ' ';
         } else {
             return $scope.allUsers[t].callMe;
         }
-    }
+    };
     $scope.showExpl = function(e) {
-        console.log('GIT GUD', e)
         if (e.type == 'mouseover') {
             $scope.explOn = true;
             $('#explBox').css({
                 'left': (e.clientX - 75) + 'px',
                 'top': (e.clientY - 50) + 'px'
-            })
+            });
         } else {
             $scope.explOn = false;
         }
-    }
+    };
 });
 var userConst = function(name, x, y, heading, tv, vel) {
     //user constructor. 
@@ -176,5 +182,5 @@ var userConst = function(name, x, y, heading, tv, vel) {
     this.shotsLeft = 5; //number of shots left. Takes time to recharge.
     this.charging = 30;
     this.fireTimeLeft = 0; //if this is not zero, the fire animation is still running
-    this.score=0;//each time user gets a kill, they get +1pt;
+    this.score = 0; //each time user gets a kill, they get +1pt;
 };
